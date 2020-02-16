de2fac7f674a46a7a26abb13819446d9c47b504b
Jamiu  Ademola <jamiu@Jamius-MacBook-Pro.local>
2020-02-16 11:27:04 +0400

    First Commit

---
+++ App.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".testimonial-carousel-box").slick({
+    autoplay: true,
+    fade: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    // speed: 5000,
+    prevArrow: ".slick-prev",
+    nextArrow: ".slick-next"
+  });
+});
